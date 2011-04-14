883550675747e8bcb90a515304e1287738846df3
José Riera <yoshi@yoshiba.(none)>
2011-04-14 09:16:56 -0430

    Creacion del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IRoma::Application.config.secret_token = 'e394ca8354f6774234b33f617d07c33c94faada23afc26bcf1dd3726090f5bdc60306457cfcc1568949598a72690b4467134bbf18931638211e384dcc667b881'
